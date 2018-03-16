8e98b4a8e11071fbc42c5b076a411e505fe695ad
Ignacio Gomez <i.gomez10@uniandes.edu.co>
2018-03-15 17:37:31 -0700

    aboutUs src

--- src/components/Extra.vue
+++ src/components/Extra.vue
@@ -3,7 +3,7 @@
 
   <iframe  v-if='this.$route.path === "/FAQ" '   src="https://docs.google.com/document/d/e/2PACX-1vQWLtScqehuJAhB7Qwpzf7-xM8Zx7S4wiZ4pJoXBYwDjHP0KipPXuEhCWTAoWrcREpZkkTxEYnwxO-F/pub?embedded=true"></iframe>
   <iframe  v-if='this.$route.path === "/Instructions" '   src="https://docs.google.com/document/d/e/2PACX-1vT_4YDIUcAD8TcBpPMUWh850rQ3yAD7TyEHZYRDjp9mY3qefnbDq8Xak68DEcEhPDi12GTAAhjKke5w/pub?embedded=true"></iframe>
-  <iframe  v-if='this.$route.path === "/AboutUs" ' src='TODO'></iframe>
+  <iframe  v-if='this.$route.path === "/AboutUs" ' src="https://docs.google.com/document/d/e/2PACX-1vT_4YDIUcAD8TcBpPMUWh850rQ3yAD7TyEHZYRDjp9mY3qefnbDq8Xak68DEcEhPDi12GTAAhjKke5w/pub?embedded=true"></iframe>
 
 </transition>
 </template>
